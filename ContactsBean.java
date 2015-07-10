159b6ea001b03c9b0d95d40cca564bdf0629f8e8
Raghav Mehta <rgvmehta99@gmail.com>
2015-07-10 18:47:37 +0530

    Create ContactsBean.java

---
+++ ContactsBean.java
@@ -0,0 +1,22 @@
+package com.example.demoreadexcelfile;
+
+public class ContactsBean {
+
+	public ContactsBean(){
+	}
+	
+	String number = "";
+	String name = "";
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
